7e22e4f37cbf5e6089ebcc841470b0a1b28bc1ad
chenhui <gaischen@163.com>
2023-09-19 10:04:28 +0800

    cached generator

---
+++ middleware/generator/id_generator.go
@@ -0,0 +1,14 @@
+package generator
+
+type IDGenerator struct {
+}
+
+func (I IDGenerator) Gen(count uint32) (UniqueID, UniqueID, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (I IDGenerator) GenOne() (UniqueID, error) {
+	//TODO implement me
+	panic("implement me")
+}
