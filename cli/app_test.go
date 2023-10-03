6db11bae78c44a0f8eb3111d5fd2c25e21b2a8ad
chenhui <gaischen@163.com>
2023-10-03 09:02:57 +0800

    add test

--- cli/app_test.go
+++ cli/app_test.go
@@ -9,3 +9,11 @@ func TestApp_Run(t *testing.T) {
 	err := NewApp().Run([]string{"server", "-h"})
 	assert.Equal(t, nil, err)
 }
+
+func TestHandleAction(t *testing.T) {
+
+}
+
+func TestApp_Command(t *testing.T) {
+
+}
